0d5ad6fdb31f1514c80f9722da0f560f0a9ed529
Eric Feinstein <erichfeinstein@gmail.com>
2019-01-23 06:57:01 -0500

    corrected seed for launch

--- seed.js
+++ seed.js
@@ -28,114 +28,115 @@ async function seed() {
       username: 'eric',
       password: '123',
     }),
-    User.create({
-      username: 'jo',
-      password: '123',
-    }),
-    User.create({
-      username: 'justin',
-      password: '123',
-    }),
+    // User.create({
+    //   username: 'jo',
+    //   password: '123',
+    // }),
+    // User.create({
+    //   username: 'justin',
+    //   password: '123',
+    // }),
   ]);
-
-  const caps = await Promise.all([
-    Capture.create({
-      latitude: 40.7205,
-      longitude: -74.01,
-      radius: defaultRadius,
-    }),
-    Capture.create({
-      latitude: 40.7305,
-      longitude: -74.005,
-      radius: defaultRadius,
-    }),
-    Capture.create({
-      latitude: 40.7105,
-      longitude: -74.005,
-      radius: defaultRadius,
-    }),
-    Capture.create({
-      latitude: 41.019,
-      longitude: -73.75,
-      radius: defaultRadius,
-    }),
-    Capture.create({
-      latitude: 41.045,
-      longitude: -73.79,
-      radius: defaultRadius,
-    }),
-    Capture.create({
-      latitude: 41.038,
-      longitude: -73.77,
-      radius: defaultRadius,
-    }),
-    //FSA coords
-    Capture.create({
-      latitude: 40.707,
-      longitude: -74.011,
-      radius: defaultRadius,
-    }),
-  ]);
-
-  // Generate random coords in NYC
-  const lowerManhattan = {
-    latitude: 40.719,
-    longitude: -73.997,
-  };
-  const noho = {
-    latitude: 40.729,
-    longitude: -73.992,
-  };
-  const midtown = {
-    latitude: 40.752,
-    longitude: -73.986,
-  };
-  const bronx = {
-    latitude: 40.849,
-    longitude: -73.8788,
-  };
-  const yankeeStadium = {
-    latitude: 40.829,
-    longitude: -73.926,
-  };
-  const myRouteToFSA = {
-    latitude: 40.917,
-    longitude: -73.851,
-  };
-
-  for (let i = 0; i <= 200; i++) {
-    let coords;
-    if (i <= 30)
-      coords = randomLocation.randomCirclePoint(lowerManhattan, 1100);
-    else if (i <= 30) coords = randomLocation.randomCirclePoint(midtown, 1500);
-    else if (i <= 50) coords = randomLocation.randomCirclePoint(noho, 1500);
-    else if (i <= 100) coords = randomLocation.randomCirclePoint(bronx, 4400);
-    else if (i <= 120)
-      coords = randomLocation.randomCirclePoint(yankeeStadium, 3000);
-    else coords = randomLocation.randomCirclePoint(myRouteToFSA, 5000);
-    console.log(coords);
-
-    const cap = await Capture.create({
-      latitude: coords.latitude,
-      longitude: coords.longitude,
-      radius: defaultRadius,
-    });
-    const userId = Math.floor(Math.random() * 3) + 1;
-    await cap.setUser(userId);
-  }
   await users[0].setTeam(teams[0].id);
-  await users[1].setTeam(teams[1].id);
-  await users[2].setTeam(teams[0].id);
-  // await users[3].setTeam(teams[2].id);
-  Promise.all([
-    caps[0].setUser(users[0].id),
-    caps[1].setUser(users[1].id),
-    caps[2].setUser(users[2].id),
-    caps[3].setUser(users[2].id),
-    caps[4].setUser(users[1].id),
-    caps[5].setUser(users[2].id),
-  ]);
-  await caps[6].setUser(users[2].id);
+
+  // const caps = await Promise.all([
+  //   Capture.create({
+  //     latitude: 40.7205,
+  //     longitude: -74.01,
+  //     radius: defaultRadius,
+  //   }),
+  //   Capture.create({
+  //     latitude: 40.7305,
+  //     longitude: -74.005,
+  //     radius: defaultRadius,
+  //   }),
+  //   Capture.create({
+  //     latitude: 40.7105,
+  //     longitude: -74.005,
+  //     radius: defaultRadius,
+  //   }),
+  //   Capture.create({
+  //     latitude: 41.019,
+  //     longitude: -73.75,
+  //     radius: defaultRadius,
+  //   }),
+  //   Capture.create({
+  //     latitude: 41.045,
+  //     longitude: -73.79,
+  //     radius: defaultRadius,
+  //   }),
+  //   Capture.create({
+  //     latitude: 41.038,
+  //     longitude: -73.77,
+  //     radius: defaultRadius,
+  //   }),
+  //   //FSA coords
+  //   Capture.create({
+  //     latitude: 40.707,
+  //     longitude: -74.011,
+  //     radius: defaultRadius,
+  //   }),
+  // ]);
+
+  // // Generate random coords in NYC
+  // const lowerManhattan = {
+  //   latitude: 40.719,
+  //   longitude: -73.997,
+  // };
+  // const noho = {
+  //   latitude: 40.729,
+  //   longitude: -73.992,
+  // };
+  // const midtown = {
+  //   latitude: 40.752,
+  //   longitude: -73.986,
+  // };
+  // const bronx = {
+  //   latitude: 40.849,
+  //   longitude: -73.8788,
+  // };
+  // const yankeeStadium = {
+  //   latitude: 40.829,
+  //   longitude: -73.926,
+  // };
+  // const myRouteToFSA = {
+  //   latitude: 40.917,
+  //   longitude: -73.851,
+  // };
+
+  // for (let i = 0; i <= 200; i++) {
+  //   let coords;
+  //   if (i <= 30)
+  //     coords = randomLocation.randomCirclePoint(lowerManhattan, 1100);
+  //   else if (i <= 30) coords = randomLocation.randomCirclePoint(midtown, 1500);
+  //   else if (i <= 50) coords = randomLocation.randomCirclePoint(noho, 1500);
+  //   else if (i <= 100) coords = randomLocation.randomCirclePoint(bronx, 4400);
+  //   else if (i <= 120)
+  //     coords = randomLocation.randomCirclePoint(yankeeStadium, 3000);
+  //   else coords = randomLocation.randomCirclePoint(myRouteToFSA, 5000);
+  //   console.log(coords);
+
+  //   const cap = await Capture.create({
+  //     latitude: coords.latitude,
+  //     longitude: coords.longitude,
+  //     radius: defaultRadius,
+  //   });
+  //   const userId = Math.floor(Math.random() * 3) + 1;
+  //   await cap.setUser(userId);
+  // }
+  //
+  // await users[1].setTeam(teams[1].id);
+  // await users[2].setTeam(teams[0].id);
+  // // await users[3].setTeam(teams[2].id);
+  // Promise.all([
+  //   caps[0].setUser(users[0].id),
+  //   caps[1].setUser(users[1].id),
+  //   caps[2].setUser(users[2].id),
+  //   caps[3].setUser(users[2].id),
+  //   caps[4].setUser(users[1].id),
+  //   caps[5].setUser(users[2].id),
+  // ]);
+  // await caps[6].setUser(users[2].id);
 
   console.log(`seeded successfully`);
 }
@@ -163,7 +164,7 @@ async function generateLotsOfCaps() {
 async function runSeed() {
   console.log('seeding...');
   try {
-    // await seed();
+    await seed();
   } catch (err) {
     console.error(err);
     process.exitCode = 1;
